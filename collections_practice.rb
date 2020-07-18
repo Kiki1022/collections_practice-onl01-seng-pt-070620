6d8d30c31b2d6cff88c70201ebb5bff4bfc0731c
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-18 14:15:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+return array.sort
+end
 
+def sort_array_desc(array)
+  array.sort! { |a,b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort! { |a,b| a.length <=> b.length}
+end
+  
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1] 
+  array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  array2 = array
+  array.each {|string| string[2] = "$"}
+  array2
+end
+
+
+def find_a(array)
+array2 = array
+array2 << array.any? {|string| string[0] = "a"}
+array2
+end
